1cc7278171685254eb034c519642196c05b046af
Shuangyuan Wen <s.wen6@student.unimelb.edu.au>
2019-04-13 21:14:01 +1000

    add users db

--- models/items.js
+++ models/items.js
@@ -6,4 +6,28 @@ var itemSchema = mongoose.Schema(
         "photo":String
     }
 );
+
+var userSchema = mongoose.Schema(
+    {
+        "name": String,
+        "firstname": {
+            type: String,
+            required: false
+        },
+        "lastname": {
+            type: String,
+            required: false
+        },
+        "email": {
+            type: String,
+            required: true
+        },
+        "passwordHash": {
+            type: String,
+            required: true
+        }
+    }
+);
+
 mongoose.model('items',itemSchema);
+mongoose.model('users',userSchema);
